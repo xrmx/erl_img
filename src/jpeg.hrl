40172124f38d201f7d6ab348e6427c7b47bdb404
Alvin Wu <alvin.wu@mochimedia.com>
2012-11-02 13:09:20 +0800

    add more kind of header for jpeg identification

--- src/jpeg.hrl
+++ src/jpeg.hrl
@@ -5,6 +5,7 @@
 -define(M_SOF1, 16#FFC1).      %% N indicates which compression process
 -define(M_SOF2, 16#FFC2).      %% Only SOF0-SOF2 are now in common use
 -define(M_SOF3, 16#FFC3).
+-define(M_DHT,  16#FFC4).
 -define(M_SOF5, 16#FFC5).      %% NB: codes C4 and CC are NOT SOF markers
 -define(M_SOF6, 16#FFC6).
 -define(M_SOF7, 16#FFC7).
@@ -17,6 +18,7 @@
 -define(M_SOI,  16#FFD8).       %% Start Of Image (beginning of datastream)
 -define(M_EOI,  16#FFD9).       %% End Of Image (end of datastream)
 -define(M_SOS,  16#FFDA).       %% Start Of Scan (begins compressed data)
+-define(M_DQT,  16#FFDB).
 -define(M_JFIF, 16#FFE0).       %% Jfif marker
 -define(M_APP1, 16#FFE1).       %% Exif marker
 -define(M_COM,  16#FFFE).       %% COMment 
